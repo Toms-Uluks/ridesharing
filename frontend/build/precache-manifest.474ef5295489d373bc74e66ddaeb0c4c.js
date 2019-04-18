87ce2a490bf8fb7f8142a2fd546c144a438e4e53
Toms Uluks <ulukst@gmail.com>
2019-04-18 13:55:06 -0400

    added build

---
+++ frontend/build/precache-manifest.474ef5295489d373bc74e66ddaeb0c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "73adc9ffaf75383691c0ebbaf4019958",
+    "url": "/static/media/background.73adc9ff.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12e286212a7c190b70cd",
+    "url": "/static/js/main.ed880a8c.chunk.js"
+  },
+  {
+    "revision": "4a30aebaeb5e070c9bfd",
+    "url": "/static/js/2.fef14492.chunk.js"
+  },
+  {
+    "revision": "12e286212a7c190b70cd",
+    "url": "/static/css/main.a0b998b2.chunk.css"
+  },
+  {
+    "revision": "bc571620a13d4312eec0037c35259106",
+    "url": "/index.html"
+  }
+];
